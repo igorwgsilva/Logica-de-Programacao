fc7ac642cd3b0eef8ee182865cfd5a98bd0cd7ea
igorwgsilva <110496185+igorwgsilva@users.noreply.github.com>
2023-10-23 10:56:33 -0300

    Create 12_Incompleto.c

---
+++ Capítulo 3 - Estruturas de Controle/Exercícios Propostos/12_Incompleto.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c;
+
+  printf("Escolha\n1 - Mamífero\n2 - Ave\n3 - Réptil\n");
+  scanf("%d", &a);
+
+  printf("Escolha\n1 - Quadrúpede\n2 - Bípede\n3 - Voador\n4 - Aquático\n5 - Não-voador\n6 - Nadador\n7 - De rapina\n8 - Com casco\n9 - Carnívoros\n10 - Sem patas\n");
+  scanf("%d", &b);
+  
+  printf("Escolha\n1 - Carnívoro\n2 - Herbivoro\n3 - Onívoro\n4 - Frutívoros\n5 - Tropicais\n6 - Polares\n");
+  scanf("%d", &c);
+
+
+
+  
+
+
+  return 0;
+}
