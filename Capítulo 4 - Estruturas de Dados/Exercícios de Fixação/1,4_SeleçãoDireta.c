cda4d923f0610380ba3d8c1736394445ddb16bad
igorwgsilva <110496185+igorwgsilva@users.noreply.github.com>
2023-11-16 13:27:58 -0300

    Update 1,4_SeleçãoDireta.c

--- Capítulo 4 - Estruturas de Dados/Exercícios de Fixação/1,4_SeleçãoDireta.c
+++ Capítulo 4 - Estruturas de Dados/Exercícios de Fixação/1,4_SeleçãoDireta.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 int main(void){
-  int v[20]={20,19,18,17,16,10,14,13,12,11,10,9,8,-7,6,5,4,3,2,1}, i, j, candidato, menor, pos, troca;
+  int v[20], i, j, candidato, menor, pos, troca;
 
+  //leitura
   for(i=0;i<20;i++){
-    
+    printf("\nv[%d] = ", i);
+    scanf("%d", &v[i]);
+  }
+
+  //seleção direta
+  for(i=0;i<20;i++){
     candidato = v[i];
     pos = i;
-
+    
     for(j=i;j<20;j++){
       if(candidato > v[j]){
         candidato = v[j]; 
         pos = j;
       }
     }
+    
     troca = v[i];
     v[i] = v[pos];
     v[pos] = troca;
   }
 
+  //imprime
+  printf("\n\n");
   for(i=0;i<20;i++)
     printf("\nv[%d] = %d", i, v[i]);
   return 0;
