ec98aa47ea0fd4961461e4b712fd1c2c1aef14e2
Igor Wendling Gurgel Silva <110496185+igorwgsilva@users.noreply.github.com>
2024-01-10 12:47:07 -0300

    Update 03.c

--- Capítulo 4 - Estruturas de Dados/Exercícios Propostos/03.c
+++ Capítulo 4 - Estruturas de Dados/Exercícios Propostos/03.c
@@ -2,24 +2,24 @@
 
 int main(void){
 
-  int i, v[5];
+  int i, cont=0, aux=0, v[6];
 
-  for(i=0;i<5;i++){
+  for(i=0;i<6;i++){
     printf("\nvetor[%d] = ", i);
     scanf("%d", &v[i]);
   }
 
-  printf("\nAntes:\n");
-
-  for(i=0;i<5;i++){
-    printf("\nv[%d] = %d",i, v[i]);
+  for(i=1;i<7;i++){
+    if(v[i-1]<v[i])
+      cont++;
+    else{
+      if(cont>=aux)
+      aux = cont;
+      cont=0;
+    }
   }
 
-  printf("\nDepois:\n");
-
-  for(i=4;i>=0;i--){
-    printf("\nv[%d] = %d",i, v[i]);
-  }
+  printf("\n\tMaior sequência: %d", aux);
 
 
   return 0;
