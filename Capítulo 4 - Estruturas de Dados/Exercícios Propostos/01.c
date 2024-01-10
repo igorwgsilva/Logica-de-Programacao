5e68d2275b3798be8b134dd983a378d88591e5c1
Igor Wendling Gurgel Silva <110496185+igorwgsilva@users.noreply.github.com>
2024-01-10 12:18:56 -0300

    Update 01.c

--- Capítulo 4 - Estruturas de Dados/Exercícios Propostos/01.c
+++ Capítulo 4 - Estruturas de Dados/Exercícios Propostos/01.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main(void){
-  printf("Hello World");
+
+  int i, x=1,v[5];
+
+  for(i=0;i<5;i++){
+    printf("\nvetor[%d] = ", i);
+    scanf("%d", &v[i]);
+  }
+
+  for(i=0;i<5;i++){
+    if(i%2==0)
+      v[i]*=3;
+      else 
+      v[i]*=2;
+    printf("\n%d° posição = %d",x++, v[i]);
+  }
+
+
   return 0;
 }
